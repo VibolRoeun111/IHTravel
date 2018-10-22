63c739ce3edcb869e5d42bef12a0e4c1b26edbc4
Vibol's Macbook Pro <vibolsmacbookpro@vibolss-macbook-pro.local>
2018-10-22 14:26:57 +0700

    Initial Commit

---
+++ IHTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IHTravel
+//
+//  Created by Vibol's Macbook Pro on 10/22/18.
+//  Copyright © 2018 Vibol Roeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
